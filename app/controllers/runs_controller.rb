31bb18b480e89b0b7f55a07653499157ae1d94f1
unknown <danielbsasser@gmail.com>
2020-04-08 12:34:24 -0400

    Built Post /runs Route

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -12,8 +12,23 @@ class RunsController < ApplicationController
 
   # POST: /runs
   post "/runs" do
-    redirect "/runs"
+    if logged_in?
+      if params[:year] == "" params[:month] == "" || params[:day] == "" || params[:duration] == "" || params[:distance] == "" || params[:pace] == ""
+        redirect to "runs/new" 
+      else
+        date = Time.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+        @run = current_user.runs.build(date: date, duration: params[:duration], distanace: params[:distance], pace: params[:pace])
+        if @run.save
+          redirect.to "/runs"
+        else
+          redirect to "/runs/new"
+        end
+      end
+    else
+      reditrect to "/users/login"
+    end
   end
+
 
   # GET: /runs/5
   get "/runs/:id" do
