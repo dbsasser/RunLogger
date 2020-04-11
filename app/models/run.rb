6c231fee56869d7464e4818a8079a8a6192a2b7f
unknown <danielbsasser@gmail.com>
2020-04-11 18:05:29 -0400

    Added speed and mile_time methods

--- app/models/run.rb
+++ app/models/run.rb
@@ -4,4 +4,15 @@ class Run < ActiveRecord::Base
     def avg_speed 
         (self.distance / self.duration.to_f)*60 
     end
+
+    def speed 
+        (self.distance / self.duration.to_f)/60
+    end
+
+    def mile_time 
+        time = 1/speed 
+        Time.at(time).utc.strftime("%H:%M:%S")
+    end 
+
+
 end
