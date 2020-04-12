332d607e8792d4921a25517406559bcd9b7c6f27
unknown <danielbsasser@gmail.com>
2020-04-11 20:38:39 -0400

    Changed Math In avg_speed & avg_speed_in_mph

--- app/models/run.rb
+++ app/models/run.rb
@@ -2,11 +2,11 @@ class Run < ActiveRecord::Base
     belongs_to :user
 
     def avg_speed_in_mph
-        (self.distance / self.duration.to_f)*60 
+        ((self.distance / self.duration.to_f)*60)*60 
     end
 
     def avg_speed 
-        (self.distance / self.duration.to_f)/60
+        (self.distance / self.duration.to_f)
     end
 
     def mile_time 
