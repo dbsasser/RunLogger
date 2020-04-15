df7c537b295781484b44e9e85e9fc5afbf9cc728
unknown <danielbsasser@gmail.com>
2020-04-14 22:16:09 -0400

    Add validation to class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     has_many :runs
     has_secure_password
+
+    validates :username, :presence => true, 
+    :uniqueness => true
+    validates :email,    :presence => true,
+    :uniqueness => true
+    validates :password, :presence => true
+
 end
